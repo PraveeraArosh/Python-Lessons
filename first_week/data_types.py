d01f2110f201d32290e2fd4c26f08738718ea17c
Praveera Arosh <118259064+PraveeraArosh@users.noreply.github.com>
2024-04-21 13:00:39 +0530

    tuple print

--- first_week/data_types.py
+++ first_week/data_types.py
@@ -28,9 +28,36 @@
 
 # Tuple
 tuple1=(1,2,3,4,5)
-print(tuple1)
+# print(tuple1)
 
+is_created = False
+is_failed = True
 
+# print(is_created,type(is_created))
 
+# if 2>0:
+#     is_created=True # convert False value as True
+# print(is_created)
+
+# set1=set("Arosh","Arosh","Arosh")
+# list1=["Arosh","Arosh","Arosh"]
+
+dic={
+    "Name":"Arosh",
+    "mobile":1900,
+    "Age":19
+}
+dic2={
+    "Name":"Arosh",
+    "mobile":1900,
+    "Age":19
+}
+print(dic["Name"])
+list=[]
+# add 2 dicts into 1 list
+
+list.append(dic)
+list.append(dic2)
+print(list)
 
 
