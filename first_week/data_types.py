7847ec4ba1e7562f489da4f592178bd0a7fbcbd1
Praveera Arosh <118259064+PraveeraArosh@users.noreply.github.com>
2024-04-21 11:41:15 +0530

    trying for data types

---
+++ first_week/data_types.py
@@ -0,0 +1,23 @@
+x="python{}"
+y= '\nHello world ---'
+z= """\nHello word....""" #Doc Strings 
+# print(type(x),x)
+# checked the type
+# print(x,y,z)
+
+q= "Elephant"
+p=0
+# print(type(p))
+# print(x.format(p))
+# print (x+q+str(p))
+
+print(f"Praveera Arosh{q}")
+
+b=0.0
+print(type(b))
+
+
+
+
+
+
