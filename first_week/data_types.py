935ecaa0aca2a685fe6c903573eb916d0dee7117
Praveera Arosh <118259064+PraveeraArosh@users.noreply.github.com>
2024-04-21 12:08:02 +0530

    List update and comment

--- first_week/data_types.py
+++ first_week/data_types.py
@@ -1,24 +1,27 @@
-x="python{}"
-y= '\nHello world ---'
-z= """\nHello word....""" #Doc Strings 
-# print(type(x),x)
-# checked the type
-# print(x,y,z)
+# x="python{}"
+# y= '\nHello world ---'
+# z= """\nHello word....""" #Doc Strings 
+# # print(type(x),x)
+# # checked the type
+# # print(x,y,z)
 
-q= "Elephant"
-p=0
+# q= "Elephant"
+# p=0
 # print(type(p))
 # print(x.format(p))
 # print (x+q+str(p))
 
-print(f"Praveera Arosh{q}")
+# print(f"Praveera Arosh{q}")
 
-b=0.0
-print(type(b))
+# b=0.0
+# print(type(b))
 
-print("Git upload")
+# print("Git upload")
 
-print("New")
+# print("New")
+
+l1=[1,2,3,"arosh",0.0]
+print(l1[3])
 
 
 
