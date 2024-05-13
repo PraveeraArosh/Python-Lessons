967ce081c22a561db9d425d8a92cc1ecd7b4fb1c
Praveera Arosh <118259064+PraveeraArosh@users.noreply.github.com>
2024-05-13 12:11:52 +0530

    latest lessons

--- 12.05.2024/six.py
+++ 12.05.2024/six.py
@@ -1,3 +1,29 @@
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#
+# odd_numbers = [num for num in numbers if num % 2 != 0]
+#
+# even_numbers = [num for num in numbers if num % 2 == 0]
+#
+# print("Odd Numbers:", odd_numbers)
 
+# print("Even Numbers:", even_numbers)
 
+start_no = input("Enter start no ")
+end_no = input("Enter start no ")
+numbers = [i for i in range(int(start_no),int(end_no),1)]
+print(numbers)
 
+def checked_odd_or_even(numbers: list)-> dict:
+    odd_list = []
+    even_list = []
+    for number in numbers:
+        if float(number)%2 == 0:
+            even_list.append(number)
+        else:
+            odd_list.append(number)
+    return {
+        "odd_list ": odd_list,
+        "even_list": even_list
+    }
+
+print(checked_odd_or_even(numbers))
